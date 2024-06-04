62045783d6e1aa9355565091d0d188f168ff6048
priyaa2671 <166091279+priyaa2671@users.noreply.github.com>
2024-06-04 14:52:32 -0400

    bug fix

--- app/routes/genre.routes.js
+++ app/routes/genre.routes.js
@@ -2,23 +2,8 @@ module.exports = (app) => {
     const Genre = require("../controllers/genre.controller");
     var router = require("express").Router();
   
-    // Create a new Genre
-    router.post("/genres/", Genre.create);
-  
     // Retrieve all Genres
     router.get("/genres/", Genre.findAll);
-  
-    // Retrieve a single Genre with id
-    router.get("/genres/:id", Genre.findOne);
-  
-    // Update a Genre with id
-    router.put("/genres/:id", Genre.update);
-  
-    // Delete a Genre with id
-    router.delete("/genres/:id", Genre.delete);
-  
-    // Delete all Genres
-    router.delete("/genres/", Genre.deleteAll);
   
     app.use("/", router);
   };
