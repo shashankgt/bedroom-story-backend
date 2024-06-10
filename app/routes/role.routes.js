f4b35c21f3866be740163ef93a0e1a28c8196d3b
priyaa2671 <166091279+priyaa2671@users.noreply.github.com>
2024-06-10 14:38:26 -0400

    Add role controllers and routes for story

--- app/routes/role.routes.js
+++ app/routes/role.routes.js
@@ -1,10 +1,24 @@
 module.exports = (app) => {
-    const Role = require("../controllers/role.controller");
-    var router = require("express").Router();
-  
-    // Retrieve all Roles
-    router.get("/roles/", Role.findAll);
+  const Role = require("../controllers/role.controller");
+  var router = require("express").Router();
 
-    app.use("/", router);
-  };
-  
+  // Create a new Role
+  router.post("/roles/", Role.create);
+
+  // Retrieve all Roles
+  router.get("/roles/", Role.findAll);
+
+  // Retrieve a single Role with id
+  router.get("/roles/:id", Role.findOne);
+
+  // Update a Role with id
+  router.put("/roles/:id", Role.update);
+
+  // Delete a Role with id
+  router.delete("/roles/:id", Role.delete);
+
+  // Delete all Roles
+  router.delete("/roles/", Role.deleteAll);
+
+  app.use("/", router);
+};
